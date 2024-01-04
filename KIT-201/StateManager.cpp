0e36ae9dbe65a0bc1c3d8a2861ec21d873ecafed
Emin AÇIKGÖZ <125810299+Emin-ACIKGOZ@users.noreply.github.com>
2024-01-05 01:27:00 +0300

    Added a Pause Screen

--- KIT-201/StateManager.cpp
+++ KIT-201/StateManager.cpp
@@ -17,11 +17,43 @@ void StateManager::render(sf::RenderWindow& window) {
         window.setView(world.getCamera().getView());
         window.clear(sf::Color(0, 0, 50));
         world.render(window);
+        
+
+        if (currentState == GameState::Pause) {
+            sf::RenderTarget& target = window;
+
+            // Get the current view center and window size
+            sf::Vector2f viewCenter = target.getView().getCenter();
+            sf::Vector2u windowSize = target.getSize();
+
+            // Semi-transparent gray rectangle to cover the screen
+            sf::RectangleShape pauseOverlay(sf::Vector2f(windowSize.x, windowSize.y));
+            pauseOverlay.setFillColor(sf::Color(0, 0, 0, 125)); // Adjust the opacity here (0-255)
+            pauseOverlay.setPosition(viewCenter - sf::Vector2f(windowSize.x / 2.0f, windowSize.y / 2.0f));
+            target.draw(pauseOverlay);
+
+            sf::Font font; // Load your font
+            font.loadFromFile("Fonts/VT323.ttf"); // Load your font file
+
+            sf::Text pausedText;
+            pausedText.setFont(font);
+            pausedText.setString("PAUSED");
+            pausedText.setCharacterSize(128);
+            pausedText.setFillColor(sf::Color::White);
+            pausedText.setStyle(sf::Text::Bold);
+
+            // Center the 'PAUSED' text accurately on the current screen's center
+            sf::FloatRect textRect = pausedText.getLocalBounds();
+            pausedText.setOrigin(textRect.left + textRect.width / 2.0f,
+                textRect.top + textRect.height / 2.0f);
+
+            pausedText.setPosition(viewCenter.x  + textRect.left,
+                viewCenter.y - textRect.height / 2.0f + textRect.top);
+
+            target.draw(pausedText);
+        }
+
         window.display();
-
-     //if (currentState == GameState::Pause) {
-
-    //}
 }
 
 void StateManager::handleEvents(sf::RenderWindow& window) {
